ffd46357a1f2df0f26132f4dc5e3a34875fa463c
miniatureian <miniatureian@users.noreply.github.com>
2018-01-16 20:54:53 -0600

    ddfill button function

--- wTools.php
+++ wTools.php
@@ -18,12 +18,12 @@ function gender(){
 function locationlevel(){
 	// -> LocationLevel set: Universe Planet Country City Neighborhood Lot
 	return "<div class=\"btn-group dropright\">
-		  <button class=\"btn btn-secondary dropdown-toggle\" type=\"button\" id=\"LocationLevel\" data-toggle=\"dropdown\" value=\"Lot\">Lot<span class=\"caret\"></span></button>
+		  <button class=\"btn btn-secondary dropdown-toggle\" type=\"button\" id=\"LocationLevel\" data-toggle=\"dropdown\" value=\"M\">M<span class=\"caret\"></span></button>
 		  <div class=\"dropdown-menu btn btn-secondary align-top\" aria-labelledby=\"LocationLevel\">
 			<button type=\"button\" class=\"dropdown-item btn btn-secondary\" 
-			id=\"Universe\" value=\"LocationLevel\" onclick=\"ddfill(this)\">Universe</button>
+			id=\"Uni\" value=\"LocationLevel\" onclick=\"ddfill(this)\">Uni</button>
 			<button type=\"button\" class=\"dropdown-item btn btn-secondary\" 
-			id=\"Planet\" value=\"LocationLevel\" onclick=\"ddfill(this)\">Planet</button>
+			id=\"Plt\" value=\"LocationLevel\" onclick=\"ddfill(this)\">Plt</button>
 		  </div>
 		</div></div>";
 }
